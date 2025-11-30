02119d4e742264cc8c0dcdf04eba4f5e5ec532db
schmanyuel <238185350+schmanyuel@users.noreply.github.com>
2025-11-30 16:38:54 +0100

    changed name to camelCase

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod cli;
+use cli::CLI;
+use clap::{Parser, Subcommand, command};
+use std::path::PathBuf;
+use rpassword;
 
 
 fn main() {
-    PasswordManager::intro_animation();
+    passwordManager::intro_animation();
     println!("Hello, world!");
+
+    let input: CLI = CLI::parse();
 }
