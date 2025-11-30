9372fbb465af379c71994670ed068203bdcc020d
Stavroula Michailidis <stavroula.michailidis@yahoo.com>
2025-11-30 17:01:26 +0100

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::fs; //imports rusts file system module
-use serde_json::Value; //imports value type (represents json data)
 use clap::Parser;
+use serde_json::Value; //imports value type (represents json data)
+use std::fs; //imports rusts file system module
 
 fn main() {
-    let string_from_json = fs::read_to_string("src/passwords_file.json").expect("could not read file");
+    let string_from_json =
+        fs::read_to_string("src/passwords_file.json").expect("could not read file");
     let json_data: Value = serde_json::from_str(&string_from_json).expect("invalid json");
 
-    println!("{json_data}"); 
-
-
+    println!("{json_data}");
 }
