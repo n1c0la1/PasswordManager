79e92ea4e5521ac78a942db76c7b0c6eebd930ea
schmanyuel <238185350+schmanyuel@users.noreply.github.com>
2025-11-30 17:17:06 +0100

    merged main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+mod cli;
+
 use clap::Parser;
 use serde_json::Value; //imports value type (represents json data)
 use std::fs; //imports rusts file system module
+use cli::CLI;
+use std::path::PathBuf;
+
 
 fn main() {
     let string_from_json =
@@ -8,4 +13,9 @@ fn main() {
     let json_data: Value = serde_json::from_str(&string_from_json).expect("invalid json");
 
     println!("{json_data}");
+
+    password_manager::intro_animation();
+    println!("Hello, world!");
+
+    let input: CLI = CLI::parse();
 }
