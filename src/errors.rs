1cdeece2e50f3c3954e494b01364340c7240b6ac
Stavroula Michailidis <stavroula.michailidis@yahoo.com>
2025-12-18 16:12:20 +0100

    divided vault_manager in 3 files, handled master securely

---
+++ src/errors.rs
@@ -0,0 +1,101 @@
+use std::{any, fmt};
+use std::error::Error;
+
+//use crate::crypto::CryptoError;
+
+#[derive(Debug)]
+pub enum CryptoError {
+    CouldNotEncrypt,
+    CouldNotDecrypt,
+}
+
+impl fmt::Display for CryptoError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CryptoError::CouldNotEncrypt => write!(f, "COULD NOT ENCRYPT"),
+            CryptoError::CouldNotDecrypt => write!(f, "COULD NOT DECRYPT"),
+        }}}
+
+
+#[derive(Debug)]
+pub enum VaultError {
+    InvalidKey,
+    NameExists,
+    FileExists,
+    PasswordTooLong,
+    EntryNotFound,
+    CouldNotSave,
+    CouldNotClose,
+    CouldNotGetEntry,
+    CouldNotRemoveEntry,
+    ConversionFailedJSON,
+    NoVaultOpen,
+    IoError(std::io::Error),
+    SerdeError(serde_json::Error),
+    EncFileError(enc_file::EncFileError),
+    AnyhowError(anyhow::Error),
+    Utf8Error(std::str::Utf8Error),
+    CryptoError(CryptoError),
+}
+
+impl fmt::Display for VaultError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VaultError::InvalidKey => write!(f, "INVALID KEY"),
+            VaultError::NameExists => write!(f, "NAME ALREADY EXISTS"),
+            VaultError::FileExists => write!(f, "FILENAME ALREADY EXISTS"),
+            VaultError::PasswordTooLong => write!(f, "PASSWORD TOO LONG"),
+            VaultError::EntryNotFound => write!(f, "ENTRY NOT FOUND"),
+            VaultError::CouldNotSave => write!(f, "COULD NOT SAVE VAULT"),
+            VaultError::CouldNotClose => write!(f, "COULD NOT CLOSE VAULT"),
+            VaultError::CouldNotGetEntry => write!(f, "COULD NOT GET ENTRY"),
+            VaultError::CouldNotRemoveEntry => write!(f, "COULD NOT REMOVE ENTRY"),
+            VaultError::ConversionFailedJSON => write!(f, "CONVERSION TO/FROM JSON FAILED"),
+            VaultError::NoVaultOpen => write!(f, "NO VAULT IS OPEN"),
+            VaultError::IoError(e) => write!(f, "IO ERROR: {}", e),
+            VaultError::SerdeError(e) => write!(f, "SERDE ERROR: {}", e),
+            VaultError::EncFileError(e) => write!(f, "ENC FILE ERROR: {}", e),
+            VaultError::AnyhowError(e) => write!(f, "ANYHOW ERROR: {}", e),
+            VaultError::Utf8Error(e) => write!(f, "UTF8 ERROR: {}", e),
+            VaultError::CryptoError(e) => write!(f, "CRYPTO ERROR: {}", e),
+        }
+    }
+}
+
+impl Error for VaultError {}
+
+impl From<std::io::Error> for VaultError {
+    fn from(error: std::io::Error) -> Self {
+        VaultError::IoError(error)
+    }
+}
+
+impl From<serde_json::Error> for VaultError {
+    fn from(error: serde_json::Error) -> Self {
+        VaultError::SerdeError(error)
+    }
+}
+
+impl From<enc_file::EncFileError> for VaultError {
+    fn from(error: enc_file::EncFileError) -> Self {
+        VaultError::EncFileError(error)
+    }
+}
+
+impl From<anyhow::Error> for VaultError {
+    fn from(error: anyhow::Error) -> Self {
+        VaultError::AnyhowError(error)
+    }
+}
+
+impl From<std::str::Utf8Error> for VaultError {
+    fn from(error: std::str::Utf8Error) -> Self {
+        VaultError::Utf8Error(error)
+    }
+}
+
+impl From<CryptoError> for VaultError {
+    fn from(error:CryptoError) -> Self {
+        VaultError::CryptoError(error)
+    }
+}
---
+++ src/vault_file_manager.rs
@@ -0,0 +1,117 @@
+//coordinates crypto.rs and vault_entry_manager
+
+/*what belongs here: 
+- Create new vault
+- Open existing vault
+- Close vault
+- Read/write encrypted files
+-knows file names and paths
+
+*/
+use secrecy::SecretString;
+use std::fs::{self, File}; 
+use std::io::{Read, Write}; 
+use std::path::Path;
+use std::str;
+
+use crate::crypto;
+use crate::vault_entry_manager::{Vault};
+use crate::errors::VaultError;
+
+//----------------------------------------------------------------------------
+// Public functions
+//----------------------------------------------------------------------------
+
+pub fn initialize_vault(name: String) -> Result<Vault, VaultError> {
+   // if key.len() > 200 {
+   //    return Err(VaultError::PasswordTooLong); 
+   // }
+
+   //IMPORTANT: password length should be checked at input (cli.rs)
+    let path = format!("vaults/{name}.psdb");
+    if Path::new(&path).exists() {
+        return Err(VaultError::FileExists);
+    }
+    let vault: Vault = Vault::new(name); //deleted mut 
+    Ok(vault)
+}
+
+//encrypts file with a master password -> use session.rs to remember the master password temporarily. must always be called with the correct master from the session
+pub fn close_vault(vault: &Vault, password: SecretString) -> Result<(), VaultError>{
+    let encrypted_vault = crypto::encrypt_vault(&password, vault.to_json())?;
+    let path = format!("vaults/{}.psdb", vault.name);
+    let mut file = File::create(path)?;
+    file.write_all(&encrypted_vault)?;
+    Ok(())
+}
+
+//opens the vault + checks if master password was correct by successfully encrypting the file
+pub fn open_vault(file_name: String, password: SecretString) -> Result<Vault, VaultError>{
+    let path = format!("vaults/{file_name}.psdb");
+
+    let encrypted_bytes = read_file_to_bytes(&path)?;
+    //handle error: enc_file::EncFileError::Crypto to validate password
+    //match error Crypto -> return Err(InvalidKey)
+    let decrypted_json = crypto::decrypt_vault(password, &encrypted_bytes)?;
+    let vault = vault_from_json(&decrypted_json)?; //deleted mut
+    Ok(vault)
+}
+
+/// Checks if any vaults exists in the vault folder.
+pub fn check_vaults_exist () -> bool {
+    fs::read_dir("vaults")
+        .map(|mut entries| entries.any(|e| {
+            e.map(|entry| {
+                entry.path().extension()
+                    .and_then(|ext| ext.to_str())
+                    .map(|ext| ext == "psdb")
+                    .unwrap_or(false)
+            }).unwrap_or(false)
+        }))
+        .unwrap_or(false)
+}
+
+pub fn change_master_pw(vault_name: String, old_password: SecretString, new_password: SecretString) -> Result<(), VaultError>{
+    let opening_vault = open_vault(vault_name, old_password)
+        .map_err(|_| VaultError::InvalidKey)?;
+    
+    let _ = close_vault(&opening_vault, new_password)
+        .map_err(|_| VaultError::CouldNotClose);
+
+    Ok(())  
+}
+
+pub fn list_vaults() -> Result<Vec<String>, VaultError> {
+    let mut vector = Vec::new();
+    let entries = fs::read_dir("vaults")?;
+
+
+    for entry in entries {
+        let unwraped_entry = entry?;
+        let entry_path = unwraped_entry.path();
+
+        if let Some(file_name) = entry_path.file_name().and_then(|s| s.to_str()) {
+            let string_entry = file_name.to_string();
+            vector.push(string_entry);
+        }
+        
+    }
+    Ok(vector)
+}
+
+//----------------------------------------------------------------------------
+// Internal helper functions (private)
+//----------------------------------------------------------------------------
+
+fn read_file_to_bytes(path: &str) -> Result<Vec<u8>, VaultError> {
+    let mut file = File::open(path)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+    Ok(contents)
+}
+
+fn vault_from_json(input: &str) -> Result<Vault, serde_json::Error> {
+    serde_json::from_str(input)
+}
+
+
