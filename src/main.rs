05853f4bc7710d30845e9e27bec1d1102a1f3b02
schmanyuel <238185350+schmanyuel@users.noreply.github.com>
2026-01-29 15:41:19 +0100

    Fix session_guard assignment in command handler

--- src/main.rs
+++ src/main.rs
@@ -294,7 +294,7 @@ fn main() {
                     }
                     match handle_command_change_master(&mut *session_guard) {
                         Ok(()) => {
-                            try_save(&mut *session_guard);
+                            *session_guard = None;
                         }
                         Err(e) => {
                             println!("Error: {}", e);
